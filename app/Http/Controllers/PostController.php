b0d846313d64b9e8010e1d0a9804640e5000f886
dyna2002chhan <dyna.chhan@student.passerellesnumeriques.org>
2021-10-27 11:18:24 +0700

    initalize project

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Post::with(['user'])->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $post = new Post();
+        $post-> title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return response()->json(['Post' => 'created'],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Profile::with('user')->findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $post = new Post();
+        $post-> title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return response()->json(['Post' => 'edited', 'data'=> $item],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $isDelete = Post::destroy($id){
+            if($isDeleted =1){
+                return response()->json(['message' => 'deleted'],200);
+            }else{
+                return response()->json(['message' => 'id not found'], 404);
+            }
+        }
+    }
+}
